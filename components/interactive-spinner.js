39abf89d3da16169ceb3c1e7ec7a6e129b4df834
aziaziazi <aziaziazi@gmail.com>
2025-05-30 23:43:05 +0200

    feat: click and right click on the loader to change speed

--- components/interactive-spinner.js
+++ components/interactive-spinner.js
@@ -5,6 +5,11 @@ class InteractiveSpinnerComponent extends HTMLElement {
             this.append(document.createElement('div'));
         }
         this.update();
+        this.addEventListener('click', this.handleClick)
+        this.addEventListener('contextmenu', e => {
+            e.preventDefault();
+            this.handleClick(false)
+        })
     }
 
     static get observedAttributes() {
@@ -23,6 +28,24 @@ class InteractiveSpinnerComponent extends HTMLElement {
         if (div) {
             div.className = 'x-spinner';
             div.style.borderTopColor = this.getAttribute('color') || null;
+        }
+    }
+    
+    handleClick(accelerate = true) {
+        const div = this.querySelector('div');
+
+        if (div) {
+            const previousDuration = window
+                .getComputedStyle(div)
+                .getPropertyValue('animation-duration')
+            
+            const nextDuration = accelerate
+                ? parseFloat(previousDuration) * 0.75
+                : parseFloat(previousDuration) / 0.75
+            
+            if (nextDuration > 0.1 && nextDuration < 10) {
+                div.style.animationDuration = nextDuration + 's';
+            }
         }
     }
 }
